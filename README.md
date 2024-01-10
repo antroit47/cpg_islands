59c5414b4d396e2464d4f0f079e4001bacbe38d8
Mike <antroit@seznam.cz>
2024-01-10 15:00:48 +0100

    add rolling window object

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# cpg_islands
+# CpG Islands
 
+This is a prototype implementation of the [Takai and Jones’ algorithm](https://www.pnas.org/doi/10.1073/pnas.052410099) to find CpG islands. 
 
+## Usage
+
+The scripts is controlled through constants at its beginning. The algorithm itself comes fully parametrized, including the nucleotide content of the islands (normally C and G in this order). The algorithm defaults are taken from its original specification.
+
+#### Program parameters
+* `NCBI_LINK` - Link to the NCBI file server
+* `GENOME_NAME` - Name of the genome on the NCBI server, which will get downloaded and analyzed
+
+#### Algorithm parameters
+
+* `MIN_WINDOW_SIZE` - Minimal bp size of the window that can be considered an island (default `200`)
+* `MIN_GC_PERCENTAGE` - Minimal required nucleotide content of prefered (default `0.5`)
+* `MIN_OBSERVED_TO_EXPECTED_CPG` - Minimal required value of observed to expected CpGs (default `0.6`)
+* `MIN_ISLAND_MERGE_GAP` - Minimal bp gap between two islands which will cause them to merge into one (default `100`)
+* `FIRST_NUCLEOTIDE` - Fist nucleotide of the island (default `"C"`)
+* `SECOND_NUCLEOTIDE` - Second nucleotide of the island (default `"G"`)
+
+## Notes
+
+- The original specification of the algorithm is rather ambiguous, but this implementation sticks to the specification in as literal sense as possible and all the following notes are related to the for this implementation and the original specification.
 - If there are multiple records returned form the NCBI, the computation is only done for the first one.
+- The algorithm, as per the specification drops small islands near the minimal window size in search of bigger ones. It can also shrink islands sligthly. Consequently, its results are not comparable to the [naive implementation](https://www.bioinformatics.org/sms2/cpg_islands.html), which just finds islands of length 200, even if we merge these islands in case they are next to each other.
+- Merging windows with small gaps in between may result in the larger windows not meeting the simple window criteria, but being windows because of the condition that connecting windows breached by a reasonable gap results in a window. 
--- islands.py
+++ islands.py
@@ -7,37 +7,155 @@ import logging
 
 NCBI_LINK = "https://www.ncbi.nlm.nih.gov/search/api/download-sequence/?db=nuccore&id="
 
+# genome name to download and analyze from NCBI
+# GENOME_NAME = "DQ011153.1"  # Monkeypox, 0.44 sec
+GENOME_NAME = "NC_060948.1"  # Human, 125 sec
 
-GENOME_NAME = "DQ011153.1"  # Monkeypox, 0.44 sec
-# GENOME_NAME = "NC_060948.1"  # Human, 125 sec
-
-# algorithm parameters
+# algorithm parameters explained in README.md
 MIN_WINDOW_SIZE = 200
 MIN_GC_PERCENTAGE = 0.5
 MIN_OBSERVED_TO_EXPECTED_CPG = 0.6
 MIN_ISLAND_MERGE_GAP = 100
+FIRST_NUCLEOTIDE = "C"
+SECOND_NUCLEOTIDE = "G"
 
 
-class Island:
+class Window:
+    """Sliding window of the CpG island search algorithm."""
+
     def __init__(
         self,
-        interval_begin: int,
-        interval_end: int,
-        gc_perc: float,
-        obs_exp: float,
-        win_length: int,
+        record: SeqRecord,
+        window_begin: int,
+        window_size: int,
     ):
-        self.interval_begin = interval_begin
-        self.interval_end = interval_end
-        self.gc_perc = gc_perc
-        self.obs_exp = obs_exp
-        self.win_length = win_length
+        self.record = record
+        self.record_len = len(record)
+        self.window_begin = window_begin
+        self.window_size = window_size
+        self.window_end = window_begin + window_size
+        self.gc_count = 0
+        self.obs_cpg = 0
+        self.gc_perc = 0
+        self.obs_exp = 0
+        self.update_gc_count()
 
     def __str__(self):
-        return f"Island at ({self.interval_begin}, {self.interval_end})\tgcper:{self.gc_perc}\tobs_exp:{self.obs_exp}\twin length:{self.win_length}"
+        return f"Window at ({self.window_begin}, {self.window_end})\tgcper:{self.gc_perc}\tobs_exp:{self.obs_exp}\twin length:{self.window_size}"
+
+    def is_island(self) -> bool:
+        """Evaluate the current window and return True if it fits the definition of an island."""
+        self._evaluate()
+        return self.gc_perc > MIN_GC_PERCENTAGE and self.obs_exp > MIN_OBSERVED_TO_EXPECTED_CPG
+
+    def _evaluate(self):
+        self.gc_perc = self.gc_count / self.window_size
+        exp_cpg = ((self.gc_count / 2) ** 2) / self.window_size
+        try:
+            self.obs_exp = self.obs_cpg / exp_cpg
+        except ZeroDivisionError:
+            self.obs_exp = 0.0
+
+    def update_gc_count(self):
+        """Count the nucleotide content in the current window and reevaluate relevant statistics."""
+        record_cut = self.record[self.window_begin : self.window_end]
+        self.gc_count = record_cut.count(FIRST_NUCLEOTIDE) + record_cut.count(SECOND_NUCLEOTIDE)
+        self.obs_cpg = record_cut.count(f"{FIRST_NUCLEOTIDE}{SECOND_NUCLEOTIDE}")
+        self._evaluate()
+
+    def shrink_left(self):
+        """Shrinks window by 1 bp from the 5' end."""
+        if self.record[self.window_begin] in [FIRST_NUCLEOTIDE, SECOND_NUCLEOTIDE]:
+            self.gc_count -= 1
+        if (
+            self.record[self.window_begin] == FIRST_NUCLEOTIDE
+            and self.record[self.window_begin + 1] == SECOND_NUCLEOTIDE
+        ):
+            self.obs_cpg -= 1
+        self.window_begin += 1
+
+    def expand_right(self) -> bool:
+        """Expands window by 1 bp to 3' end,
+
+        :return: False if the record end is reached.
+        """
+        if self.window_end >= self.record_len:
+            return False
+        if self.record[self.window_end] in [FIRST_NUCLEOTIDE, SECOND_NUCLEOTIDE]:
+            self.gc_count += 1
+        if (
+            self.record[self.window_end - 1] == FIRST_NUCLEOTIDE
+            and self.record[self.window_end] == SECOND_NUCLEOTIDE
+        ):
+            self.obs_cpg += 1
+        self.window_end += 1
+        return True
+
+    def shrink_right(self):
+        """Shrinks window by 1 bp from the 3' end."""
+        if self.record[self.window_end - 1] in [FIRST_NUCLEOTIDE, SECOND_NUCLEOTIDE]:
+            self.gc_count -= 1
+        if (
+            self.record[self.window_end - 2] == FIRST_NUCLEOTIDE
+            and self.record[self.window_end - 1] == SECOND_NUCLEOTIDE
+        ):
+            self.obs_cpg -= 1
+        self.window_end -= 1
+
+    def expand_left(self):
+        """Expands window by 1 bp to 5' end."""
+        if self.record[self.window_begin - 1] in [FIRST_NUCLEOTIDE, SECOND_NUCLEOTIDE]:
+            self.gc_count += 1
+        if (
+            self.record[self.window_begin - 1] == FIRST_NUCLEOTIDE
+            and self.record[self.window_begin] == SECOND_NUCLEOTIDE
+        ):
+            self.obs_cpg += 1
+        self.window_begin -= 1
+
+    def expand_right_island(self) -> bool:
+        """Expands window by minimal window size to the 3' end. It creates new window, checks if
+        it's an island and joins it to this island if so.
+
+        :return:  True if this window was extended, false otherwise or at the end of record.
+        """
+        if not (remaining_window_length := self.record_len - self.window_end):
+            return False
+        if remaining_window_length < MIN_WINDOW_SIZE:
+            logging.debug("reached end of sequence in big jump, be careful")
+            new_window_begin = self.record_len - MIN_WINDOW_SIZE
+        else:
+            new_window_begin = self.window_end
+        next_window = Window(self.record, new_window_begin, MIN_WINDOW_SIZE)
+        self.window_end = new_window_begin + MIN_WINDOW_SIZE
+        self.window_size = self.window_end - self.window_begin
+        return next_window.is_island()
+
+    def rollback_until_island(self):
+        """Rollback the last sub-window of minimal window size to the 5' end until the sub-window
+        is an island.
+        """
+        last_window = Window(self.record, self.window_end - MIN_WINDOW_SIZE, MIN_WINDOW_SIZE)
+        while not last_window.is_island():
+            last_window.expand_left()
+            last_window.shrink_right()
+        self.join_window(last_window)
+
+    def join_window(self, other_window):
+        """Expands this window so that its end is and the end of the other_window. Recalculates the
+        nucleotide content of the merged window.
+        """
+        self.window_end = other_window.window_end
+        self.window_size = self.window_end - self.window_begin
+        self.update_gc_count()
 
 
 def get_sequence_from_ncbi(genome_id: str) -> Seq:
+    """Retrieves record from NCBI file server given its name.
+
+    :raises: Exception if the genome is not found on the server.
+    """
+    print("Pulling data from NCBI")
     response = requests.get(NCBI_LINK + genome_id)
     genome = response.content.decode()
     if genome.startswith("Error"):
@@ -48,253 +166,127 @@ def get_sequence_from_ncbi(genome_id: str) -> Seq:
     return records[0].seq
 
 
-def find_islands_simple(record: SeqRecord) -> List[Island]:
-    if (record_length := len(record)) < MIN_WINDOW_SIZE:
-        return []
-    found_islands = []
-    window_position = 0
+def find_island_1bp_shifts(window: Window) -> bool:
+    """Searches the record in 1bp increments until the first island is found.
 
-    window = record[window_position : window_position + MIN_WINDOW_SIZE]
-    gc_count = window.count("G") + window.count("C")
-    obs_cpg = window.count("CG")
+    :return: False if end of record is reached before island is found, True otherwise.
+    """
     while True:
-        gc_perc = gc_count / MIN_WINDOW_SIZE
-        exp_cpg = ((gc_count / 2) ** 2) / MIN_WINDOW_SIZE
-        try:
-            obs_exp = obs_cpg / exp_cpg
-        except ZeroDivisionError:
-            obs_exp = 0.0
-        if gc_perc > MIN_GC_PERCENTAGE and obs_exp > MIN_OBSERVED_TO_EXPECTED_CPG:
-            found_islands.append(
-                Island(
-                    interval_begin=window_position,
-                    interval_end=window_position + MIN_WINDOW_SIZE,
-                    gc_perc=gc_perc,
-                    obs_exp=obs_exp,
-                    win_length=MIN_WINDOW_SIZE,
-                )
-            )
-
-        window_position += 1
-        if window_position + MIN_WINDOW_SIZE > record_length:
-            break
-
-        window = record[window_position : window_position + MIN_WINDOW_SIZE]
-        # update CGs
-        if record[window_position - 1] in ["C", "G"]:
-            gc_count -= 1
-        if window[MIN_WINDOW_SIZE - 1] in ["C", "G"]:
-            gc_count += 1
-        # update CpGs
-        if record[window_position - 1] == "C" and window[0] == "G":
-            obs_cpg -= 1
-        if window[MIN_WINDOW_SIZE - 2] == "C" and window[MIN_WINDOW_SIZE - 1] == "G":
-            obs_cpg += 1
-
-    return found_islands
-
-
-def find_island_1bp_shifts(
-    record_position: int, record: SeqRecord, min_window_size: int, record_length: int
-) -> int:
-    window = record[record_position : record_position + min_window_size]
-    gc_count, obs_cpg = new_window_gcs(window)
-    while True:
-        gc_perc, obs_exp, is_island = island_evaluation(gc_count, obs_cpg, min_window_size)
-        if is_island:
-            logging.debug(f"island found {record_position, record_position + min_window_size}")
-            return record_position
-        if record_position + min_window_size + 1 > record_length:
+        if window.is_island():
+            logging.debug(f"island found {window.window_begin, window.window_end}")
+            return True
+        if not window.expand_right():
             logging.debug("reached the end of record")
-            return -1
-        gc_count, obs_cpg, record_position = shift_window_1bp(
-            gc_count, obs_cpg, record, record_position
-        )
+            return False
+        window.shrink_left()
 
 
-def new_window_gcs(window):
-    gc_count = window.count("G") + window.count("C")
-    obs_cpg = window.count("CG")
-    return gc_count, obs_cpg
+def two_fold_shrinking(window: Window) -> bool:
+    """Shrinks big windows from both sides until they meet island criteria.
+
+    :return: True if shrunk window is an island, False if shrinking results in a window smaller
+    than minimal window size.
+    """
+    while not window.is_island():
+        window.shrink_right()
+        window.shrink_left()
+        window.window_size -= 2
+        if window.window_size < MIN_WINDOW_SIZE:
+            logging.debug("! Shrinking resulted in window smaller than minimal window size.")
+            return False
+    return True
 
 
-def find_islands(record: SeqRecord, min_window_size: int) -> List[Island]:
-    found_islands = []
-    if (record_length := len(record)) < min_window_size:
-        return found_islands
-
-    record_position = 0
-    while True:
-        # step 1 find window that is an island
-        logging.debug("- Stage 1: 1bp shifts")
-        record_position = find_island_1bp_shifts(
-            record_position, record, min_window_size, record_length
-        )
-        if record_position == -1:
-            break
-
-        # step 2 window-length shifts
-        logging.debug("- Stage 2: window-length shifts")
-        island_start = record_position
-        record_position += min_window_size  # perform a shift and evaluate the following windows
-        island_end = extend_island_window_shifts(record, record_length, record_position)
-        logging.debug(f"island: {island_start}, {island_end}")
-
-        # step 3 shift the last window by 1bp until it meets the criteria
-        logging.debug("- Stage 3: 1bp rollback")
-        window = record[island_end - min_window_size : island_end]
-        gc_count, obs_cpg = new_window_gcs(window)
-
-        for i in range(1, min_window_size):
-            if record[island_end - i] in ["C", "G"]:
-                gc_count -= 1
-            if record[island_end - min_window_size - i] in ["C", "G"]:
-                gc_count += 1
-            if record[island_end - i - 2 : island_end - i] == "CG":
-                obs_cpg -= 1
-            if (
-                record[island_end - min_window_size - i - 2 : island_end - min_window_size - i]
-                == "CG"
-            ):
-                obs_cpg += 1
-            gc_perc, obs_exp, is_island = island_evaluation(gc_count, obs_cpg, MIN_WINDOW_SIZE)
-            if is_island:
-                logging.debug(f"island ending shrunk by {i}")
-                break
-        island_end = island_end - i
-        logging.debug(f"Updated island after 1bp shrinking {island_start}, {island_end}")
-
-        # step 4 shrink the whole island by 1bp until it meets the criteria
-        logging.debug("- Stage 4 1bp two-fold shrinking")
-        window = record[island_start:island_end]
-        gc_count, obs_cpg = new_window_gcs(window)
-
-        while True:
-            gc_perc, obs_exp, is_island = island_evaluation(
-                gc_count, obs_cpg, island_end - island_start
-            )
-            if is_island:
-                break
-            if record[island_start] in ["C", "G"]:
-                gc_count -= 1
-            if record[island_end] in ["C", "G"]:
-                gc_count -= 1
-            if record[island_start : island_start + 1] == ["CG"]:
-                gc_count -= 1
-            island_start += 1
-            island_end -= 1
-        logging.debug(f"updated island after shrinking: {island_start}, {island_end}")
-
-        found_islands.append(
-            Island(
-                interval_begin=island_start,
-                interval_end=island_end,
-                gc_perc=gc_perc,
-                obs_exp=obs_exp,
-                win_length=island_end - island_start,
-            )
-        )
-        record_position = island_end
-
-    logging.debug("-- INITIAL SCAN COMPLETE")
-
-    # step 5 at the end merge islands that are at least 100bp apart
-    logging.debug("- Stage 5 merging islands")
-    merged_islands = []
-
-    # TODO testing bloat remove
-    found_islands.append(
-        Island(
-            interval_begin=189260, interval_end=189480, gc_perc=0.51, obs_exp=0.9, win_length=200
-        )
-    )
-    found_islands.append(
-        Island(
-            interval_begin=189570, interval_end=189770, gc_perc=0.51, obs_exp=0.9, win_length=200
-        )
-    )
-    found_islands.append(
-        Island(
-            interval_begin=189990, interval_end=190300, gc_perc=0.51, obs_exp=0.9, win_length=200
-        )
-    )
-
+def merge_islands(found_islands: List[Window]) -> List[Window]:
+    """Merge islands in the :param: found_islands that are close to each other."""
+    merged_islands = [found_islands[0]]
     if len(found_islands) >= 2:
-        merged_islands.append(found_islands[0])
-
-        for found_island in found_islands:
-            if found_island.interval_begin - merged_islands[-1].interval_end < MIN_ISLAND_MERGE_GAP:
+        for found_island in found_islands[1:]:
+            if found_island.window_begin - merged_islands[-1].window_end < MIN_ISLAND_MERGE_GAP:
                 logging.debug(
-                    f"Merged Islands at {merged_islands[-1].interval_end}, {found_island.interval_end}"
+                    f"Merged Islands at {merged_islands[-1].window_begin}, "
+                    f"{merged_islands[-1].window_end}, {found_island.window_begin}, "
+                    f"{found_island.window_end}"
                 )
-                merged_islands[-1].interval_end = found_island.interval_end
+                merged_islands[-1].join_window(found_island)
             else:
                 merged_islands.append(found_island)
     return merged_islands
 
 
-def extend_island_window_shifts(record, record_length, record_position):
+def extend_island_window_shifts(window: Window):
+    """Expands window by minimal window size increments to the 3' end as long as the added windows
+    are islands.
+    """
     while True:
-        real_window_length = min(MIN_WINDOW_SIZE, record_length - 1 - record_position)
-        if real_window_length < 2:
-            logging.warning("reached end of sequence in big jump")
-            island_end = record_position
+        extended = window.expand_right_island()
+        if not extended:
             break
-        window = record[record_position:real_window_length]
-        gc_count, obs_cpg = new_window_gcs(window)
-        gc_perc, obs_exp, is_island = island_evaluation(gc_count, obs_cpg, real_window_length)
-        record_position += real_window_length
-        if not is_island:
-            logging.debug("window is no longer an island")
-            island_end = record_position
+
+    window.update_gc_count()
+    logging.debug(f"island: {window.window_begin}, {window.window_end}")
+
+
+def find_islands(record: SeqRecord) -> List[Window]:
+    """Finds CpG islands in the given record.
+
+    :return: List of found islands
+    """
+    found_islands = []
+    record_position = 0
+    while True:
+        window = Window(record, record_position, MIN_WINDOW_SIZE)
+
+        # step 1 find window that is an island
+        logging.debug("- Stage 1: 1bp shifts")
+        record_continues = find_island_1bp_shifts(window)
+        if not record_continues:
             break
-    return island_end
 
+        # step 2 window-length shifts
+        logging.debug("- Stage 2: window-length shifts")
+        extend_island_window_shifts(window)
 
-def island_evaluation(gc_count, obs_cpg, window_size) -> (float, float, bool):
-    is_island = False
-    gc_perc = gc_count / window_size
-    exp_cpg = ((gc_count / 2) ** 2) / window_size
-    try:
-        obs_exp = obs_cpg / exp_cpg
-    except ZeroDivisionError:
-        obs_exp = 0.0
-    if gc_perc > MIN_GC_PERCENTAGE and obs_exp > MIN_OBSERVED_TO_EXPECTED_CPG:
-        is_island = True
-    return gc_perc, obs_exp, is_island
+        # step 3 shift the last window by 1bp until it meets the criteria
+        logging.debug("- Stage 3: 1bp rollback")
+        window.rollback_until_island()
+        logging.debug(
+            f"Updated island after 1bp shrinking {window.window_begin}, {window.window_end}"
+        )
 
+        # step 4 shrink the whole island by 1bp until it meets the criteria
+        logging.debug("- Stage 4 1bp two-fold shrinking")
+        if not two_fold_shrinking(window):
+            record_position = window.window_begin + 1
+            continue
+        logging.debug(f"Updated island after shrinking: {window.window_begin}, {window.window_end}")
 
-def shift_window_1bp(
-    gc_count: int, obs_cpg: int, record: SeqRecord, window_position: int
-) -> (int, int, int):
-    window_position += 1
-    window = record[window_position : window_position + MIN_WINDOW_SIZE]
-    # update CGs
-    if record[window_position - 1] in ["C", "G"]:
-        gc_count -= 1
-    if window[MIN_WINDOW_SIZE - 1] in ["C", "G"]:
-        gc_count += 1
-    # update CpGs
-    if record[window_position - 1] == "C" and window[0] == "G":
-        obs_cpg -= 1
-    if window[MIN_WINDOW_SIZE - 2] == "C" and window[MIN_WINDOW_SIZE - 1] == "G":
-        obs_cpg += 1
-    return gc_count, obs_cpg, window_position
+        found_islands.append(window)
+        record_position = window.window_end
+
+    # step 5 at the end merge islands that are at least 100bp apart
+    logging.debug("- Stage 5 merging islands")
+    if not found_islands:
+        return []
+
+    merged_islands = merge_islands(found_islands)
+    return merged_islands
 
 
 def main():
-    logging.debug = print  # comment for debug logs
+    # logging.debug = print  # uncomment for debug logs
     record = get_sequence_from_ncbi(GENOME_NAME)
+
     print(f"record len: {len(record)}")
 
     start = time.time()
-    islands = find_islands(record, MIN_WINDOW_SIZE)  # time: 0.3477661609649658  ->
+    islands = find_islands(record)
     end = time.time()
 
     for island in islands:
         print(island)
 
-    print(f"Time: {end - start}")
+    logging.debug(f"Time: {end - start}")
     print("Done")
 
 
